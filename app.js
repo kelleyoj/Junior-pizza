a607b118db3f685df40ddd1c267a43462e8c6c2f
OJ <kelleyoj@gmail.com>
2019-02-19 17:00:09 -0500

    add footer

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dotenv = require('dotenv').config();
 
@@ -18,8 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', indexRouter);
+app.use('/', require('./routes/index'));
 app.use('/users', usersRouter);
+app.use('/menu', require('./routes/menu'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
